062bd8d29cae2ea5159dfef846902e5c7706eba8
andrew-paterson <andrew.paterson9@gmail.com>
2021-07-02 16:44:22 +0200

    Add create release script

---
+++ create-release.js
@@ -0,0 +1,34 @@
+const tagAndRelease  = require('tag-and-release');
+const pandagoCommitMessage = getNamedArgVal('--pg_commit_msg');
+const hugoCommitMessage = getNamedArgVal('--hs_commit_msg');
+const fs = require('fs');
+
+(async function () {
+  const res = await tagAndRelease.run({repo: '../hugo-sundries', commitMessage: hugoCommitMessage});
+  const pandogoModFileUpdated = fs.readFileSync('./go.mod', 'utf-8').split('\n').map(line => {
+    if (line.indexOf('require github.com/andrew-paterson/hugo-sundries') > -1) {
+      line = `require github.com/andrew-paterson/hugo-sundries ${res.results.tag} // indirect`
+    }
+    return line;
+  }).join('\n');
+  fs.writeFileSync('./go.mod', pandogoModFileUpdated);
+  console.log('test');
+  const final = await tagAndRelease.run({commitMessage: pandagoCommitMessage});
+  console.log(final)
+
+})()
+
+// tagAndRelease.run({commitMessage: 'New commit'})
+function getNamedArgVal(requested) {
+  const [ , , ...args ] = process.argv;
+  var val;
+  args.forEach(arg => {
+    if (arg.indexOf('=') < 0) { return; }
+    var argName = arg.split('=')[0];
+    if (argName === requested) {
+      val = arg.split('=')[1];
+    }
+  });
+  return val;
+}
+
