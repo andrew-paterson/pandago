669e6184b43ec207c9e5658a39e87404915aad26
Andrew Paterson <andrew.paterson9@gmail.com>
2021-12-17 14:14:25 +0200

    Various refactoring

--- assets/js/mobile-nav-toggler.js
+++ assets/js/mobile-nav-toggler.js
@@ -1,14 +1,17 @@
 var mobileNavToggler = document.querySelector('.mobile-nav-toggler');
-mobileNavToggler.addEventListener('click', () => {
-  var menuHeight = document.querySelector('ul.menu').clientHeight;
-  if (mobileNavToggler.classList.contains('active')) {
-    document.querySelector('nav.main .nav-ul-container').style.height = '0';
-    mobileNavToggler.classList.remove('active');
-  } else {
-    document.querySelector('nav.main .nav-ul-container').style.height = `${menuHeight}px`;
-    mobileNavToggler.classList.add('active');
-  }
-});
+if (mobileNavToggler) {
+  mobileNavToggler.addEventListener('click', () => {
+    var menuHeight = document.querySelector('ul.menu').clientHeight;
+    if (mobileNavToggler.classList.contains('active')) {
+      document.querySelector('nav.main .nav-ul-container').style.height = '0';
+      mobileNavToggler.classList.remove('active');
+    } else {
+      document.querySelector('nav.main .nav-ul-container').style.height = `${menuHeight}px`;
+      mobileNavToggler.classList.add('active');
+    }
+  });
+}
+
 
 
 
